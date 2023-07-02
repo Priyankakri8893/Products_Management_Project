32466ec707d4af5760c47b0670774c543c5be093
Priyanka <priyankakri8893@gmail.com>
2023-07-02 14:00:14 +0530

    auth

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const router= express.Router()
+const {auth}= require("../middlewares/auth")
 const {userRegister, userLogin, getUser, updateUser}= require("../controllers/userController")
 
 router.post("/register", userRegister)
