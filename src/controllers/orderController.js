e4f75904ef01b8fcb6027c7846d2169d3e7fe7de
Priyanka <priyankakri8893@gmail.com>
2023-07-10 15:10:17 +0530

    testing of order api

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -3,6 +3,7 @@ const cartModel= require('../models/cartModel')
 const productModel= require('../models/productModel')
 const orderModel= require('../models/orderModel')
 const {isValid}= require('../validation/isvalid')
+const {mongoose}= require('mongoose')
 
 const orders= async (req, res) => {
     try {
@@ -33,19 +34,33 @@ const orders= async (req, res) => {
         }
 
         //authorization check here
-        if(req["x-api-key"].userId != userIdExit._id){
-            return res.status(403).send({
-                status: false,
-                message: "unauthorized, userId not same"
+        // if(req["x-api-key"].userId != userIdExit._id){
+        //     return res.status(403).send({
+        //         status: false,
+        //         message: "unauthorized, userId not same"
+        //     })
+        // }
+
+        let {cartId}= req.body
+        if(!isValid(cartId)){
+            return res.status(404).send({
+                status: false, 
+                message: "cartId is invalid"
             })
         }
 
+        if(!mongoose.isValidObjectId(cartId)){
+            return res.status(400).send({
+                status: false,  
+                message: "cartId is not valid"
+            }) 
+        }
         //check cart exit or not
-        let cartExit= await cartModel.findById({userId: userId})
-        if(!cartExit){
+        let cartExit= await cartModel.findOne({userId: userId, _id: cartId})
+        if(!cartExit || cartExit.items.length == 0){
             return res.status(404).send({
                 status: false,
-                message: 'cart not exit by this userId'
+                message: 'first create cart then order it'
             })
         }
 
@@ -55,7 +70,7 @@ const orders= async (req, res) => {
         }
         const order= {
             userId: userId,
-            items: cartExit.items,
+            items: [...cartExit.items],
             totalPrice: cartExit.totalPrice,
             totalItems: cartExit.totalItems,
             totalQuantity: totalQuantity
@@ -63,7 +78,12 @@ const orders= async (req, res) => {
 
         let createOrder= await orderModel.create(order)
 
-        res.status(201).send({
+        cartExit.items= []
+        cartExit.totalItems= 0
+        cartExit.totalPrice= 0
+        await cartExit.save()
+
+        res.status(200).send({
             status: true,
             message: 'product is successfully order',
             data: createOrder
@@ -107,15 +127,73 @@ const updateOrders= async (req, res) => {
         }
 
         //authorization check here
-        if(req["x-api-key"].userId != userIdExit._id){
-            return res.status(403).send({
-                status: false,
-                message: "unauthorized, userId not same"
+        // if(req["x-api-key"].userId != userIdExit._id){
+        //     return res.status(403).send({
+        //         status: false,
+        //         message: "unauthorized, userId not same"
+        //     })
+        // }
+
+        let {orderId, status}= req.body
+
+        if(!isValid(orderId)){
+            return res.status(404).send({
+                status: false, 
+                message: "orderId is invalid"
             })
         }
 
+        if(!mongoose.isValidObjectId(orderId)){
+            return res.status(400).send({
+                status: false,  
+                message: "orderId is not valid"
+            }) 
+        }
 
+        let statusOption= ['pending', 'completed', 'canceled']
+        if(!status || !statusOption.includes(status)){
+            return res.status(400).send({
+                status: false,  
+                message: "provide valid status"
+            })
+        }
 
+        let orderExit= await orderModel.findOne({_id: orderId, userId: userId, isDeleted: false})
+
+        if(!orderExit){
+            return res.status(404).send({
+                status: false,  
+                message: "order not exit"
+            })
+        }
+
+        if(status == 'canceled'){
+            if(orderExit.cancellable == false){
+                return res.status(400).send({
+                    status: false,  
+                    message: "order is already canceled"
+                })
+            }
+        }
+
+        if(status == 'canceled'){
+            if(orderExit.cancellable == true){
+                orderExit.status= 'canceled'
+                orderExit.cancellable= false
+                orderExit.deletedAt= Date.now()
+                orderExit.isDeleted= true
+            }
+        }else{
+            orderExit.status= status
+        }
+
+        let updateOrderStatus= await orderExit.save()
+
+        res.status(200).send({
+            status: true,
+            message: 'status of order is updated successfully',
+            data: updateOrderStatus
+        })
     } catch (error) {
         res.status(500).send({
             status: false,
